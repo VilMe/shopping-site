0517353e6d2dfc2e36d0819f697492635f1278e4
Vilma M <vilma.medrano00@gmail.com>
2019-03-27 13:29:20 -0700

    create customer object, TO DO function for ready customer.txt file

--- customers.py
+++ customers.py
@@ -4,4 +4,30 @@
 class Customer(object):
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__ (self,
+                  self.first_name,
+                  self.last_name,
+                  self.email,
+                  self.password
+                  ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        """convenience method to show information about 
+        customer object in console
+        """
+
+        return "<Customer: Name is {} {} Email is {}>".format(self.first_name,
+            self.last_name, self.email)
+
+
+def read_customers_from_file(filepath):
+    """ Read customer information file and populate 
+    dictionary of customer
+    
+    """
+
+
